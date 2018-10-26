ef836e11c1d2266c3578673fbe194505dd9658a4
Egor Gavrilov <Reideenek@gmail.com>
2018-10-26 22:58:05 +0400

    will bot work?

--- bot.py
+++ bot.py
@@ -21,17 +21,7 @@ def main():
 
 
     def textMessage(bot, update):
-        request = apiai.ApiAI(os.environ.get('DF_KEY', None)).text_request()  # Токен API к Dialogflow
-        request.lang = 'ru'  # На каком языке будет послан запрос
-        request.session_id = 'superbotinok'  # ID Сессии диалога (нужно, чтобы потом учить бота)
-        request.query = update.message.text  # Посылаем запрос к ИИ с сообщением от юзера
-        responseJson = json.loads(request.getresponse().read().decode('utf-8'))
-        response = responseJson['result']['fulfillment']['speech']  # Разбираем JSON и вытаскиваем ответ
-        # Если есть ответ от бота - присылаем юзеру, если нет - бот его не понял
-        if response:
-            bot.send_message(chat_id=update.message.chat_id, text=response)
-        else:
-            bot.send_message(chat_id=update.message.chat_id, text='Пожалуйста подождите, скоро мы ответим на ваш вопрос!')
+        bot.send_message(chat_id=update.message.chat_id, text='привет!')
 
 
     # Хендлеры
